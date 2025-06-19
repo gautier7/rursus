4cfdbf2ce1576af09d28ffdfba9ef0b0a63938fa
gautier7 <gautier-colson@hotmail.com>
2025-06-19 22:59:49 +0800

    Initial Commit

---
+++ Rursus/RursusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RursusApp.swift
+//  Rursus
+//
+//  Created by Gautier Colson on 19/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RursusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
